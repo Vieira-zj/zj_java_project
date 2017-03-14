6cf54b51b5d126df3fbdce39fe39d57dfa58c8c9
vieira <zivieira@163.com>
2017-03-14 21:55:44 +0800

    Add java demos.

--- src/com/zhengjin/java/demo/EnumStatus.java
+++ src/com/zhengjin/java/demo/EnumStatus.java
@@ -1,6 +1,10 @@
 package com.zhengjin.java.demo;
 
 public enum EnumStatus {
+	// 枚举中还有一个ordinal()方法返回一个int值，这是每个enum实例在声明时的次序，从0开始。
+	// 枚举类还实现了Compareable接口，所以他具有compareTo()方法。
+	// 同时还实现了Serializable接口，还自动为你提供了equals()和hashCode()方法。
+	// 除了不能继承一个枚举类之外，我们基本上可以把枚举类当成一个常规的Java类，可以往其中添加新的方法，包括抽象方法甚至main方法。
 
 	DELETE("0", "DEL"), ENABLE("1", "AVAILABLE"), DISABLE("2", "UNAVAILABLE");
 
--- src/com/zhengjin/java/demo/TestDemo03.java
+++ src/com/zhengjin/java/demo/TestDemo03.java
@@ -1,15 +1,141 @@
 package com.zhengjin.java.demo;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+
+import static com.zhengjin.apis.testutils.TestUtils.printLog;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public final class TestDemo03 {
 
 	@Test
 	public void test01Demo() {
+		// Arrays.equals()
+		String[] tmpArrSrc = { "test1", "test2", "test3" };
+		String[] tmpArrTarget = new String[] { "test1", "test2", "test3" };
+		boolean ret = Arrays.equals(tmpArrSrc, tmpArrTarget);
+		printLog("Compare results: " + ret);
+	}
+
+	@Test
+	public void test02Demo() {
+		// Arrays.copyOf()
+		String[] tmpArr = { "1", "2", "3", "4", "5", "6", "7", "8" };
+		tmpArr = Arrays.copyOf(tmpArr, 5);
+		for (String item : tmpArr) {
+			printLog(item);
+		}
+	}
+
+	@Test
+	public void test03Demo() {
+		// string reverse
+		StringBuilder sb = new StringBuilder("abcd");
+		printLog(sb.reverse().toString());
+	}
+
+	@Test
+	public void test04Demo() {
 		// System.arraycopy()
+		String[] arrSrc = { "1", "2", "3", "4", "5", "6", "7", "8" };
+		String[] arrTarget = { "a", "b", "c", "d" };
+		for (String item : arrTarget) {
+			printLog(item);
+		}
+
+		// Copy 4 elements from array 'arrSrc' starting at offset 3 to array
+		// 'arrTarget' starting at offset 0
+		printLog("After update: ");
+		System.arraycopy(arrSrc, 3, arrTarget, 0, 4);
+		for (String item : arrTarget) {
+			printLog(item);
+		}
+	}
+
+	@Test
+	public void test05Demo() {
+		// reflection
+		String tmpStr = "Zheng Jin";
+		try {
+			Field valueFieldOfString = String.class.getDeclaredField("value");
+			valueFieldOfString.setAccessible(true);
+			char[] value = (char[]) valueFieldOfString.get(tmpStr);
+			value[5] = '_';
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		printLog("After updated, tmpStr = " + tmpStr);
+	}
+
+	@Test
+	public void test06Demo() {
+		// serialized
+		Employee employee = new Employee();
+		employee.name = "Reyan Ali";
+		employee.address = "Phokka Kuan, Ambehta Peer";
+		employee.SSN = 11122333;
+		employee.number = 101;
+
+		try {
+			FileOutputStream fileOut = new FileOutputStream("d:\\employee.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(employee);
+			out.close();
+			fileOut.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void test07Demo() {
+		// de-serialized
+		Employee employee = null;
+
+		FileInputStream fileIn;
+		try {
+			fileIn = new FileInputStream("d:\\employee.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			employee = (Employee) in.readObject();
+			in.close();
+			fileIn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		printLog("Deserialized Employee...");
+		printLog("Name: " + employee.name);
+		printLog("Address: " + employee.address);
+		printLog("SSN: " + employee.SSN);
+		printLog("Number: " + employee.number);
+	}
+
+	@SuppressWarnings({ "serial", "unused" })
+	private static class Employee implements Serializable {
+
+		public String name;
+		public String address;
+		public transient int SSN;
+		public int number;
+
+		public void mailCheck() {
+			printLog("Mailing a check to " + name + " " + address);
+		}
+	}
+
+	@Test
+	public void test20Demo() {
+		// TODO
 	}
 
 }
